d84bb77448c0cff29370942f575e7544f6940045
Calvin <calvinhlee23@gmail.com>
2016-07-18 15:45:01 -0700

    p02 done

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,20 +8,38 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    return true if @already_built_response
+    false
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    unless already_built_response?
+      @res.header["location"] = "http://www.google.com"
+      @res.status = 302
+      @already_built_response = "redirect_to #{url}"
+    else
+      raise "you cannot redirect more than once"
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    if @already_built_response
+      raise "you cannot render more than once"
+    else
+      @already_built_response = content
+    end
+    @res['Content-Type'] = content_type
+    @res.body = [content]
   end
 
   # use ERB and binding to evaluate templates
@@ -37,4 +55,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
